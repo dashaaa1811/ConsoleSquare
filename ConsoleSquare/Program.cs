87d37659a31bcea0476dc4c8af1b48aec527c059
Dasha <darabrueva65@gmail.com>
2025-03-08 11:12:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+                int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(Square(num));
+        }
+        static int Square(int a)
+        {
+            int s = a * a;
+            return s;
+        }
+    }
+}
